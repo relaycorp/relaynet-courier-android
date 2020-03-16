bc13007a4827a2177357f7dcaccbc2c432ee2618
Sérgio Santos <me@sergiosantos.info>
2020-03-16 17:47:48 +0000

    JUnit5 for unit tests

--- app/src/test/java/tech/relaycorp/courier/ExampleUnitTest.kt
+++ app/src/test/java/tech/relaycorp/courier/ExampleUnitTest.kt
@@ -1,13 +1,11 @@
 package tech.relaycorp.courier
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 class ExampleUnitTest {
-  @Test
-  fun `Some good addition`() {
-    assertEquals(4, 2 + 2)
-  }
+    @Test
+    fun `Some good addition`() {
+        assertEquals(4, 2 + 2)
+    }
 }
