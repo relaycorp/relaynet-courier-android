d243ee633054968d0e32a71e3b4f778aff656d4e
Gus Narea <gnarea@users.noreply.github.com>
2020-05-20 14:55:01 +0100

    Implement upload signing

--- .github/scripts/publish-to-play-store.sh
+++ .github/scripts/publish-to-play-store.sh
@@ -7,8 +7,10 @@ set -o pipefail
 
 VERSION_NAME="$1"
 
-# We need to store the GCP Service Account credentials in a temporary file...
-# https://github.com/Triple-T/gradle-play-publisher/issues/546#issuecomment-630978695
+# We have to pass some secrets to Gradle and we'll pass as many as we can as environment variables,
+# but some must be passed as file paths. To avoid leaking those files, we'll use process
+# substitution.
 ./gradlew publish \
   -PversionName="${VERSION_NAME}" \
+  -PsigningKeystorePath=<(echo "${ANDROID_KEYSTORE}") \
   -PgcpServiceAccountCredentials=<(echo "${GCP_SERVICE_ACCOUNT}")
