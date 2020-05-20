44c51602ab2f5072da613a2b3e470d521236284f
Gus Narea <gnarea@users.noreply.github.com>
2020-05-20 19:28:58 +0100

    fix: Enable automatic releases to Google Play (alpha track)

--- .github/scripts/publish-to-play-store.sh
+++ .github/scripts/publish-to-play-store.sh
@@ -11,6 +11,7 @@ VERSION_NAME="$1"
 # but some must be passed as file paths. To avoid leaking those files, we'll use process
 # substitution.
 ./gradlew publish \
+  -PenableGpp=true \
   -PversionName="${VERSION_NAME}" \
   -PsigningKeystorePath=<(echo "${ANDROID_KEYSTORE}") \
   -PgcpServiceAccountCredentials=<(echo "${GCP_SERVICE_ACCOUNT}")
