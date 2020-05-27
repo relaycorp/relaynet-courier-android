544ad243de3236c263b23ce6a9a5fc2784e3c049
Sérgio Santos <me@sergiosantos.info>
2020-05-27 11:51:40 +0100

    About section

--- app/src/main/java/tech/relaycorp/courier/ui/settings/SettingsActivity.kt
+++ app/src/main/java/tech/relaycorp/courier/ui/settings/SettingsActivity.kt
@@ -3,6 +3,7 @@ package tech.relaycorp.courier.ui.settings
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -10,15 +11,18 @@ import com.mikepenz.aboutlibraries.LibsBuilder
 import com.stationhead.android.shared.viewmodel.ViewModelFactory
 import kotlinx.android.synthetic.main.activity_settings.deleteData
 import kotlinx.android.synthetic.main.activity_settings.innerContainer
+import kotlinx.android.synthetic.main.activity_settings.knowMore
 import kotlinx.android.synthetic.main.activity_settings.licenses
 import kotlinx.android.synthetic.main.activity_settings.storageAvailable
 import kotlinx.android.synthetic.main.activity_settings.storageMaxSlider
 import kotlinx.android.synthetic.main.activity_settings.storageMaxValue
 import kotlinx.android.synthetic.main.activity_settings.storageTotal
 import kotlinx.android.synthetic.main.activity_settings.storageUsed
+import kotlinx.android.synthetic.main.activity_settings.version
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.take
+import tech.relaycorp.courier.BuildConfig
 import tech.relaycorp.courier.R
 import tech.relaycorp.courier.ui.BaseActivity
 import tech.relaycorp.courier.ui.common.Insets.addSystemWindowInsetToPadding
@@ -44,6 +48,10 @@ class SettingsActivity : BaseActivity() {
         setupNavigation(R.drawable.ic_close)
         innerContainer.addSystemWindowInsetToPadding(bottom = true)
 
+        version.text =
+            getString(R.string.about_version, BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE)
+
+        knowMore.setOnClickListener { openKnowMore() }
         licenses.setOnClickListener { openLicenses() }
         deleteData.setOnClickListener { openDeleteDataDialog() }
 
@@ -55,11 +63,7 @@ class SettingsActivity : BaseActivity() {
         viewModel
             .storageStats()
             .onEach {
-                storageUsed.text = getString(
-                    R.string.value_with_percentage,
-                    it.used.format(this),
-                    it.usedPercentage
-                )
+                storageUsed.text = it.used.format(this)
                 storageAvailable.text = it.available.format(this)
                 storageTotal.text = it.total.format(this)
             }
@@ -89,6 +93,10 @@ class SettingsActivity : BaseActivity() {
         super.onDestroy()
         deleteDataDialog?.dismiss()
         deleteDataDialog = null
+    }
+
+    private fun openKnowMore() {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.relaynet_website))))
     }
 
     private fun openLicenses() {
