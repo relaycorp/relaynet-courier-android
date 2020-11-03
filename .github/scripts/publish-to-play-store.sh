3e333c82910da1340854744a34e45190895427d5
Gus Narea <gnarea@users.noreply.github.com>
2020-11-03 09:53:05 +0000

    fix(release): Upgrade GPP to v3 to fix broken automated releases on Gradle 6.5

--- .github/scripts/publish-to-play-store.sh
+++ .github/scripts/publish-to-play-store.sh
@@ -9,9 +9,7 @@ VERSION_NAME="$1"
 
 set +x  # Disable debug (if enabled) so we won't leak secrets accidentally
 echo "${ANDROID_KEYSTORE}" | base64 -d > /tmp/keystore.jks
-echo "${GCP_SERVICE_ACCOUNT}" > /tmp/gcp-service-account.json
 exec ./gradlew app:publish \
   -PenableGpp=true \
   -PversionName="${VERSION_NAME}" \
-  -PsigningKeystorePath=/tmp/keystore.jks \
-  -PgcpServiceAccountCredentials=/tmp/gcp-service-account.json
+  -PsigningKeystorePath=/tmp/keystore.jks
